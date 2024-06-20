4bf3d2e36288d6e760c4779482869fd0a90436a9
Alexandre Rayol <alexandrerayol.dev@gmail.com>
2024-06-20 18:45:12 -0300

    feat: finishes OrdersTable

--- components/orders-table.tsx
+++ components/orders-table.tsx
@@ -1,3 +1,4 @@
+'use client'
 import {
   Table,
   TableBody,
@@ -7,58 +8,93 @@ import {
   TableRow,
 } from '@/components/ui/table';
 import { Badge } from './ui/badge';
-import { ChevronsUpDown } from 'lucide-react';
+import { ChevronDown, ChevronUp, ChevronsUpDown } from 'lucide-react';
+import { Order } from '@/lib/@types/order';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
-export default function OrdersTable() {
+interface OrdersTableProps {
+  orders: Order[]
+}
+
+const formatter = new Intl.NumberFormat('pt-br', {
+  style: 'currency',
+  currency: 'BRL'
+}).format
+
+export default function OrdersTable({orders}:OrdersTableProps) {
+  const searchParams = useSearchParams()
+  const path = usePathname()
+  const { replace } = useRouter()
+  const params = new URLSearchParams(searchParams)
+
+  function handleClick(key: string){
+    const sort = params.get('sort')
+
+    if(sort === key){
+      params.set('sort', `-${key}`)
+    } else if(sort === `-${key}`){
+      params.delete('sort')
+    } else if(sort === null){
+      params.set('sort', key)
+    } else if(sort){
+      params.set('sort', key)
+    }
+
+    replace(`${path}?${params.toString()}`)
+  }
+
+  function getSortIcon(key: string){
+    const sort = params.get('sort')
+
+    if(sort === key){
+      return <ChevronDown className='w-4'/>
+    } else if(sort === `-${key}`){
+      return <ChevronUp className='w-4'/>
+    }
+
+    return <ChevronsUpDown className='w-4'/>
+  }
+
   return (
     <Table>
       <TableHeader>
         <TableRow className="w-full">
           <TableHead className="table-cell">Cliente</TableHead>
           <TableHead className="table-cell">Status</TableHead>
-          <TableHead className="table-cell cursor-pointer justify-end items-center gap-1">
+          <TableHead className="table-cell cursor-pointer justify-end items-center gap-1" onClick={() => handleClick('order_date')}>
             <div className="flex items-center gap-1">
               Data
-              <ChevronsUpDown className="w-4" />
+              {getSortIcon('order_date')}
             </div>
           </TableHead>
-          <TableHead className="text-right cursor-pointer flex justify-end items-center gap-1">
+          <TableHead className="text-right cursor-pointer flex justify-end items-center gap-1" onClick={() => handleClick('amount_in_cents')}>
             Valor
-            <ChevronsUpDown className="w-4" />
+            {getSortIcon('amount_in_cents')}
           </TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        <TableRow>
-          <TableCell>
-            <div className="font-medium">Fulano de Tal</div>
-            <div className="hidden md:inline text-sm text-muted-foreground">
-              fulano.de.tal@gmail.com
-            </div>
-          </TableCell>
-          <TableCell>
-            <Badge className={`text-xs`} variant="outline">
-              Pendente
-            </Badge>
-          </TableCell>
-          <TableCell className="hidden md:table-cell">2024-01-01</TableCell>
-          <TableCell className="text-right">R$100,00</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>
-            <div className="font-medium">Ciclana de Tal</div>
-            <div className="text-sm text-muted-foreground">
-              ciclana.de.tal@gmail.com
-            </div>
-          </TableCell>
-          <TableCell>
-            <Badge className={`text-xs`} variant="outline">
-              Completo
-            </Badge>
-          </TableCell>
-          <TableCell className="hidden md:table-cell">2023-01-01</TableCell>
-          <TableCell className="text-right">R$500,00</TableCell>
-        </TableRow>
+        {
+          orders.map( (order) => {
+            return (
+              <TableRow key={order.id}>
+                <TableCell>
+                  <div className="font-medium">{order.customer_name}</div>
+                  <div className="hidden md:inline text-sm text-muted-foreground">
+                  {order.customer_email}
+                  </div>
+                </TableCell>
+                <TableCell>
+                  <Badge className={`text-xs`} variant="outline">
+                  { order.status === 'pending' ? 'Pendente' : 'Completo' }
+                  </Badge>
+                </TableCell>
+                <TableCell className="hidden md:table-cell">{order.order_date.toString()}</TableCell>
+                <TableCell className="text-right">{formatter(order.amount_in_cents / 100)}</TableCell>
+              </TableRow>
+            )
+          })
+        }
       </TableBody>
     </Table>
   );
